64b3730f25659f8a34340bfa4c8e174aab26e041
david-blg <david.stack.dev@gmail.com>
2024-05-31 17:53:34 +0200

    chore: Bump version to 0.6.4 and remove commented code in AppRoutes.tsx

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -2,7 +2,7 @@ import CostExplorerPage from "@/pages/CostExplorerPage"
 import DashboardPage from "@/pages/DashboardPage"
 import DownloadPage from "@/pages/DownloadPage"
 import MetricsPage from "@/pages/MetricsPage"
-import NotFoundPage from "@/pages/NotFoundPage"
+// import NotFoundPage from "@/pages/NotFoundPage"
 import SettingsPage from "@/pages/SettingsPage"
 import StackPage from "@/pages/StackPage"
 import UploadPage from "@/pages/UploadPage"
@@ -19,7 +19,7 @@ const AppRoutes = () => {
                 <Route path="/stack" element={<StackPage />} />
                 <Route path="/cost-explorer" element={<CostExplorerPage />} />
                 <Route path="/settings" element={<SettingsPage />} />
-                <Route path="*" element={<NotFoundPage />} />
+                {/* <Route path="*" element={<NotFoundPage />} /> */}
             </Routes>
         </>
     )
