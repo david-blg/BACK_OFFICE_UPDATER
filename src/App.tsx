3dfbf029bdf9571c0ca7b13672ed0ccdf0febe80
david-blg <david.stack.dev@gmail.com>
2024-06-01 12:04:44 +0200

    update version to 0.6.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import AppLayout from "./layouts/AppLayout";
 import AppRoutes from "./routes/AppRoutes";
 import { handleUpdates } from "./updater/updater";
-// import { handleUpdates } from "./updater/updater";
 
 
 function App() {
 
-  // useEffect(() => {
-  //   handleUpdates();
-  // }, []);
+    useEffect(() => {
+      console.log('useEffect is called');
+      const setupUpdates = async () => {
+        const unlisten = await handleUpdates();
+        
+        // Cleanup function to unlisten to the updater events
+        return () => {
+          unlisten();
+        };
+      };
+      
+      setupUpdates();
+  
+    }, []);
 
-  handleUpdates();
 
 
   return (
