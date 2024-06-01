f72791b16536d4baf28f45ff32a75771dab68604
david-blg <david.stack.dev@gmail.com>
2024-06-01 11:37:31 +0200

    chore: Remove commented code in App.tsx and update handleUpdates() call

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 import AppLayout from "./layouts/AppLayout";
 import AppRoutes from "./routes/AppRoutes";
 import { handleUpdates } from "./updater/updater";
+// import { handleUpdates } from "./updater/updater";
 
 
 function App() {
 
-  useEffect(() => {
-    handleUpdates();
-  }, []);
+  // useEffect(() => {
+  //   handleUpdates();
+  // }, []);
+
+  handleUpdates();
 
 
   return (
